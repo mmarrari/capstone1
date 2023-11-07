16b50988aee3f9d4819887be9c907717263e7300
Martín Marrari <mmarrari@een.com>
2023-11-07 12:41:59 -0300

    Adding booking

--- LittleLemon/restaurant/urls.py
+++ LittleLemon/restaurant/urls.py
@@ -1,7 +1,13 @@
-#define URL route for index() view
-from django.urls import path
+# define URL route for index() view
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path("", views.index, name="index"),
+    path("menu/<int:pk>/", views.SingleMenuItemView.as_view()),
+    path('booking/', include(router.urls)),
 ]
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
 
-# Create your views here.
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
+
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, "index.html", {})
+
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    lookup_field = "pk"
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
